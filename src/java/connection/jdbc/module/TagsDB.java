4c954ad6036b6fdc0e413fd390de18a1e9d75157
partho99 <partho.swe@gmail.com>
2019-05-11 17:37:22 +0600

    tagsDB.java added and fetching object from database

---
+++ src/java/connection/jdbc/module/TagsDB.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connection.jdbc.module;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import pojo.java.module.Contents;
+
+/**
+ *
+ * @author partho
+ */
+public class TagsDB extends JdbcDao {
+
+    public ArrayList<Contents> findByTags(String tagsName) throws SQLException, ClassNotFoundException {
+
+        ArrayList<Contents> contentList = new ArrayList<>();
+
+        openConnection();
+        String sql = "select contents.content_id,contents.content_title,contents.content_text,contents.creation_dtm,"
+                + "contents.update_dtm,contents.image_path,contents.is_active,"
+                + "tags.tag_id,tags.tag_title from contents left join contenttag on "
+                + "contents.content_id = contenttag.content_id right join tags on "
+                + "contenttag.tag_id = tags.tag_id where tag_title='" + tagsName + "';";
+        System.err.println(tagsName);
+
+        ps = connection.prepareStatement(sql);
+        resultSet = ps.executeQuery();
+
+        while (resultSet.next()) {
+
+            Contents content = new Contents();
+            content.setContentId(resultSet.getInt("content_id"));
+            content.setContentText(resultSet.getString("content_text"));
+            content.setContentTitle(resultSet.getString("content_title"));
+            content.setCreationDtm(resultSet.getTimestamp("creation_dtm"));
+            content.setUpdateDtm(resultSet.getTimestamp("update_dtm"));
+            content.setImagePath(resultSet.getString("image_path"));
+            content.setIsActive(resultSet.getInt("is_active"));
+            // content.setViewsCounter(resultSet.getInt("views"));
+
+            contentList.add(content);
+        }
+        return contentList;
+
+    }
+
+}
