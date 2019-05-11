0e9f920c05f183a332a6c33e7e98661d02a449a2
partho99 <partho.swe@gmail.com>
2019-05-11 17:40:07 +0600

    tags simple logic are implented in blogcontroller

--- src/java/controller/servlet/module/BlogController.java
+++ src/java/controller/servlet/module/BlogController.java
@@ -6,9 +6,11 @@
 package controller.servlet.module;
 
 import connection.jdbc.module.ContentsDB;
+import connection.jdbc.module.TagsDB;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
@@ -16,7 +18,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import pojo.java.module.ContentTag;
 import pojo.java.module.Contents;
+import pojo.java.module.Tag;
 
 /**
  *
@@ -28,17 +32,20 @@ public class BlogController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        ArrayList<Contents> contents = new ArrayList<>();
-        ArrayList<Contents> latestpost = new ArrayList<>();
-        ContentsDB cDB = new ContentsDB();
-       
+        String tagName = request.getParameter("tagName");
+        
+        if(tagName != null){
+            ArrayList<Contents> latestpost = new ArrayList<>();
+            ArrayList<Contents> tagContents = new ArrayList<>();
+            ContentsDB cDB = new ContentsDB();
+           
+            TagsDB tagsDb = new TagsDB();
+           try {
 
-        try {
-          
-            contents = cDB.getAllContents();
+            tagContents = tagsDb.findByTags(tagName);
             latestpost = cDB.latestPost();
-
             
+
         } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(BlogController.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -46,11 +53,33 @@ public class BlogController extends HttpServlet {
 //        for (Contents content : contents) {
 //            System.out.println(content.getContentTitle());
 //        }
+        request.setAttribute("BlogContents", tagContents);
+        request.setAttribute("latestpost", latestpost);
+        request.getRequestDispatcher("blog.jsp").forward(request, response);
+        }
+        else{
+            
+        ArrayList<Contents> contents = new ArrayList<>();
+        ArrayList<Contents> latestpost = new ArrayList<>();
+        ContentsDB cDB = new ContentsDB();
 
+        try {
+
+            contents = cDB.getAllContents();
+            latestpost = cDB.latestPost();
+
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(BlogController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+//        for (Contents content : contents) {
+//            System.out.println(content.getContentTitle());
+//        }
         request.setAttribute("BlogContents", contents);
         request.setAttribute("latestpost", latestpost);
         request.getRequestDispatcher("blog.jsp").forward(request, response);
         // response.sendRedirect("blog.jsp");
+        }
 
     }
 
@@ -72,17 +101,27 @@ public class BlogController extends HttpServlet {
         String postTitle = request.getParameter("title");
 
         String postText = request.getParameter("description");
+        String tagName = request.getParameter("tag");
 
         contents.setContentTitle(postTitle);
         contents.setContentText(postText);
         contents.setCreationDtm(dt);
         contents.setUpdateDtm(dt);
         contents.setUserId(userId);
-
-        // System.out.println(postText);
-        if (saveContents.saveContents(contents) == true) {
+        
+        Tag  tag = new Tag();
+        
+        int contentId = saveContents.saveContents(contents);
+        
+        if (contentId > 0) {
+            
+            tag.setTagTitle(tagName);
+            if(saveContents.saveToContentTags(tag,contentId) == true){
+                response.sendRedirect("/software-schooling/BlogController");
+            }
+            
             // if()
-            response.sendRedirect("/software-schooling/BlogController");
+            
         }
 
     }
