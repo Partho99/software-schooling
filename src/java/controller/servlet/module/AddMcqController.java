6d8a785f623dcefcac50cfb789e5e0776d6f2810
partho99 <partho.swe@gmail.com>
2019-05-20 01:54:36 +0600

    fetching data from jsp file

--- src/java/controller/servlet/module/AddMcqController.java
+++ src/java/controller/servlet/module/AddMcqController.java
@@ -5,12 +5,14 @@
  */
 package controller.servlet.module;
 
+import connection.jdbc.module.AddMcqDB;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import pojo.java.module.MultipleChoiceQuestions;
 
 /**
  *
@@ -22,8 +24,41 @@ public class AddMcqController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String tags = request.getParameter("tags");
-        System.out.println(tags);
+        
+        String quetion = request.getParameter("question_text");
+        String tags = request.getParameter("tags");     
+        int correctChoice = Integer.parseInt( request.getParameter("correct_choice"));
+        String option1 = request.getParameter("option1");
+        String option2 = request.getParameter("option2");
+        String option3 = request.getParameter("option3");
+        String option4 = request.getParameter("option4");
+        
+        MultipleChoiceQuestions mcq = new MultipleChoiceQuestions();
+        AddMcqDB amcqbd = new AddMcqDB();
+        
+        
+        if( quetion != null && tags != null && option1 != null && option2 != null && option3 != null &&  option4 != null && correctChoice > 0){
+            
+            mcq.setQuestionText(quetion);
+            mcq.setQuestionTag(tags);
+            mcq.setCorrectAnswer(correctChoice);
+            mcq.setOption1(option1);
+            mcq.setOption2(option2);
+            mcq.setOption3(option3);
+            mcq.setOption4(option4);
+            
+            if(amcqbd.setQuestion(mcq) == true){
+                response.sendRedirect("addMcq.jsp");
+            }
+            else{
+               response.sendRedirect("index.jsp");
+            }
+            
+            
+            
+        }
+        
+        
     }
 
     
